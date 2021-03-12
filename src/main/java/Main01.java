968faa74ee39024d2fbbd5e0ffaa2d91101412c7
Tomasz Terlecki <tomaszjerzyt@gmail.com>
2021-03-12 08:57:59 +0100

    Create Main01.class

--- src/main/java/Main01.java
+++ src/main/java/Main01.java
@@ -1,8 +1,7 @@
+import java.io.InputStreamReader;
+
 public class Main01 {
     public static void main(String[] args) {
- //       KeyboardReader kr = new KeyboardReader();
-        int result = KeyboardReader.readInt(7);
-        System.out.println(result);
 
         String result1 = KeyboardReader.readString();
         System.out.println(result1);
